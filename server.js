e05a1f5d08da03ca75a17eb3bede3b934d584461
Luiz Ricardo <lricardo@outlook.com>
2019-04-09 23:21:46 -0300

    primeiros arquivos

---
+++ server.js
@@ -0,0 +1,38 @@
+const express = require('express')
+const mongoose = require('mongoose')
+const path = require('path')
+const cors = require('cors')
+
+const app = express()
+
+app.use(cors())
+
+const server = require('http').Server(app) //recebe requisições via http
+const io = require('socket.io')(server) //recebe requisições via socket
+
+io.on('connection', socket => {
+    socket.on('connectRoom', box => {
+        socket.join(box)
+    })
+})
+
+mongoose.connect('mongodb+srv://omnistack:omnistack@cluster0-cbgyw.mongodb.net/db-omnistack?retryWrites=true', {
+    useNewUrlParser: true,
+})
+
+app.use((req, res, next) => { //para atribuir o Socket.IO para toda requisição
+    req.io = io //cria um novo "padrão" = io que pode ser acessado a qualquer momento em qualquer controller
+    return next()
+})
+
+app.use(express.json())
+app.use(express.urlencoded({
+    extended: true
+}))
+
+
+app.use('/files', express.static(path.resolve(__dirname, 'temp')))
+
+app.use(require('./src/routes'))
+
+server.listen(3000)
---
+++ src/controllers/FileController.js
@@ -0,0 +1,26 @@
+const File = require('../models/Files')
+const Box = require('../models/Box')
+
+class FileController {
+    async store(req, res) {
+        const boxId = req.params.id
+        const box = await Box.findById(boxId)
+        console.log(req.file)
+
+        const file = await File.create({
+            title: req.body.title,
+            original: req.file.originalname,
+            path: req.file.key
+        })
+
+        box.files.push(file)
+
+        await box.save()
+
+        req.io.sockets.in(box._id).emit('file', file)
+
+        return res.json(file)
+    }
+}
+
+module.exports = new FileController()
